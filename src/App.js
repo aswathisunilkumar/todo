36b5c4dd9ac2c65c5d55da458d9d189c50818388
Aswathi Sunilkumar <aswathisunilkumar33@gmail.com>
2020-11-01 22:18:34 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,159 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import {v4} from 'uuid';
+
+const item = {
+  id: v4(),
+  name: "Clean the house"
+}
+
+const item2 = {
+  id: v4(),
+  name: "Wash the car"
+}
+
+
+const item3 = {
+  id: v4(),
+  name: "Clean the bus"
+}
+
+const item4 = {
+  id: v4(),
+  name: "Wash the cloth"
+}
+
 
 function App() {
+  const [text, setText] = useState("");
+  const [todoState, setTodoState] = useState({
+    "todo": {
+      title: "Todo",
+      items: [item]
+    },
+    "doing": {
+      title: "In Progress",
+      items: [item2]
+    },
+    "done": {
+      title: "Completed",
+      items: [item3]
+    }
+  })
+  // const [todo, setTodo] = useState([]);
+  // const [doing, setDoing] = useState([]);
+
+  const addTodo = () => {
+    setTodoState(prev => {
+      return {
+        ...prev,
+        todo : {
+          title : "Todo",
+          items : [
+            {
+              id : v4(),
+              name: text
+            },
+        
+          ...prev.todo.items,
+        ],
+        }
+      }   
+    }) 
+
+    setText("");
+  }
+
+  const onDragOver = (e) => {
+    e.preventDefault();
+  }
+
+  const onDragStart = (e, key, item, status) => {
+    e.dataTransfer.setData("id", key);
+    e.dataTransfer.setData("task",JSON.stringify(item));
+    e.dataTransfer.setData("status",status);
+  }
+
+  
+  const onDrop = (e, destStatus) => {
+    let taskIndex= e.dataTransfer.getData("id");
+    // let task = JSON.parse(e.dataTransfer.getData("task"));
+    let sourceStatus = e.dataTransfer.getData("status");
+    const task = {...todoState[sourceStatus].items[taskIndex]};
+    setTodoState(prevState =>{
+      prevState = {...prevState};
+      prevState[sourceStatus].items.splice(taskIndex, 1);
+      prevState[destStatus].items.splice(prevState[destStatus].items.length, 0, task);
+      // console.log(prevState);
+      
+      return prevState;
+    })
+    
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className="todo-form">
+        <input type="text" value={text} onChange={(e)=> setText(e.target.value)}/>
+        <button onClick={addTodo}>Add Todo</button>
+      </div>
+      <div className="todos">
+        <div className="task-container"
+          onDragOver={(e) => onDragOver(e)}
+          onDrop = {(e) => onDrop(e, "todo")}
         >
-          Learn React
-        </a>
-      </header>
+          {
+          todoState.todo.items.map((item,key) => {
+            return (
+              <div  key={key}
+                onDragStart = {(e) => onDragStart(e, key, item, "todo")}
+                draggable
+                className="draggable"
+              >
+                {item.name}</div>
+          ) 
+          })
+        }
+        </div>
+        <div className="droppable in-progress" 
+          onDragOver={(e) => onDragOver(e)}
+          onDrop = {(e) => onDrop(e, "doing")}
+          >
+          {
+            todoState.doing.items.map((item,key) => {
+              return (
+                <div   key={key}
+                  onDragStart = {(e) => onDragStart(e, key, item, "doing")}
+                  draggable
+                  className="draggable"
+                >
+                  {item.name}
+                </div>
+            )
+          })
+          }
+        </div>
+        <div className="droppable done" 
+          onDragOver={(e) => onDragOver(e)}
+          onDrop = {(e) => onDrop(e, "done")}
+          >
+          {
+            todoState.done.items.map((item,key) => {
+              return (
+                <div   key={key}
+                  onDragStart = {(e) => onDragStart(e, key, item, "done")}
+                  draggable
+                  className="draggable"
+                >
+                  {item.name}
+                </div>
+            )
+          })
+          }
+        </div>
+      </div>
+      
     </div>
   );
 }
